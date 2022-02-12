71c600c03989c50ae476fbbf21e22bd15238ebac
SofaGaintseva <sgaintseva@mail.ru>
2022-02-12 12:01:01 +0300

    Второе задание

--- typeof.js
+++ typeof.js
@@ -1,5 +1,18 @@
 let sofa = 'ksu'
 let sofaaa = '123'
+let soffaa = '1234'
+let soofaa = '12345'
+let ssofaa = '12'
+let soffa = '123456'
+let sooffa = '1234567'
+let sofffa = '12345678'
+
 console.log(typeof sofa,sofa);
 console.log(typeof sofaaa,sofaaa);
+console.log(typeof soffaa,soffaa);
+console.log(typeof soofaa,soofaa);
+console.log(typeof ssofaa,ssofaa);
+console.log(typeof soffa,soffa);
+console.log(typeof sooffa,sooffa);
+console.log(typeof sofffa,sofffa);   
 console.log(typeof undefined,undefined);
