67397f5d286ea930349ccbb87f0328372c50ed22
SofaGaintseva <sgaintseva@mail.ru>
2022-03-03 16:19:49 +0300

    Добавляет маршрутизацию

--- Практика 2/marsh.js
+++ Практика 2/marsh.js
@@ -1,5 +1,7 @@
 const http = require("http");
+
 const requestHandler = (request, response) => {
+    
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
     if (request.url === "/home" || request.url === "/") {
         response.write("<h2>Home</h2>");
@@ -12,4 +14,5 @@ const requestHandler = (request, response) => {
     }
     response.end();
 };
+
 http.createServer(requestHandler).listen(3000);
