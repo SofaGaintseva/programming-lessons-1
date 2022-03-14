880c538da76a6ac27b48b845fb0f909333935680
SofaGaintseva <sgaintseva@mail.ru>
2022-03-14 11:59:04 +0300

    Добавляем фото на сервер

--- images.js
+++ images.js
@@ -11,8 +11,8 @@ const requestHandler = (request, response) => {
 
 
 
-    } else if (request.url == "/image/1") {
-        let content = fs.readFileSync("1.jpg");
+    } else if (request.url == "/image/(1)") {
+        let content = fs.readFileSync("(1).jpg");
         response.writeHead(200, {
             'Content-Type': 'image/jpeg'
         })
@@ -20,48 +20,48 @@ const requestHandler = (request, response) => {
 
 
         response.end(content);
-    } else if (request.url == "/image/2") {
-        let content = fs.readFileSync("2.jpg");
+    } else if (request.url == "/image/(2)") {
+        let content = fs.readFileSync("(2).jpg");
         response.writeHead(200, {
             'Content-Type': 'image/jpeg'
         })
 
 
         response.end(content);
-    } else if (request.url == "/image/3") {
-        let content = fs.readFileSync("3.jpg");
+    } else if (request.url == "/image/(3)") {
+        let content = fs.readFileSync("(3).jpg");
         response.writeHead(200, {
             'Content-Type': 'image/jpeg'
         })
 
 
         response.end(content);
-    } else if (request.url == "/image/4") {
-        let content = fs.readFileSync("4.jpg");
+    } else if (request.url == "/image/(4)") {
+        let content = fs.readFileSync("(4).jpg");
         response.writeHead(200, {
             'Content-Type': 'image/jpeg'
         })
 
 
         response.end(content);
-    } else if (request.url == "/image/5") {
-        let content = fs.readFileSync("5.jpg");
+    } else if (request.url == "/image/(5)") {
+        let content = fs.readFileSync("(5).jpg");
         response.writeHead(200, {
             'Content-Type': 'image/jpeg'
         })
 
 
         response.end(content);
-    } else if (request.url == "/image/6") {
-        let content = fs.readFileSync("6.jpg");
+    } else if (request.url == "/image/(6)") {
+        let content = fs.readFileSync("(6).jpg");
         response.writeHead(200, {
             'Content-Type': 'image/jpeg'
         })
 
 
         response.end(content);
-    } else if (request.url == "/image/7") {
-        let content = fs.readFileSync("7.jpg");
+    } else if (request.url == "/image/(7)") {
+        let content = fs.readFileSync("(7).jpg");
         response.writeHead(200, {
             'Content-Type': 'image/jpeg'
         })
